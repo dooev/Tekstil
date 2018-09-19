71e15d7c06ce7a50dcc61f2a9cae188e4803a6e9
Dima Dooev <dooevdima3@gmail.com>
2018-09-19 11:36:12 +0300

    Портфолио, переписал тексты

--- 7_cases.php
+++ 7_cases.php
@@ -4,41 +4,60 @@
   <div class="row">
         <div class="col-12 col-md-10 offset-md-1">
           <div class="cases-slider">
-            <div class="fotorama" data-nav="thumbs" data-maxheight="85%" data-thumbwidth="115" data-thumbheight='78' data-transition="slide" data-loop="true" data-navwidth='100%' data-thumbmargin='7' data-arrows='false' data-fit='contain' data-autoplay="true">
-              <img class="fotorama__img" src="img/7_cases/111.jpg" data-caption='60 стрейч чехлов на коктельные столы <br>
+            <div class="fotorama" data-nav="thumbs" data-maxheight="85%" 
+             data-thumbwidth="115" data-thumbheight='78' data-transition="slide" data-loop="true" data-navwidth='100%' data-thumbmargin='7' data-arrows='false' data-fit='contain' data-autoplay="true">
+              <img class="fotorama__img" src="img/7_cases/111.jpg" 
+              data-caption='
+              <b>Цена: 111000 руб</b><br>
+              <b>Сроки: 2 дня</b><br>
+              	<b>Заказ: </b>60 Стрейч-Чехлов на коктельный столы + 10 Стрейч-Чехлов на фуршетные столы<br>' alt="">
+              <img class="fotorama__img" src="img/7_cases/222.jpg" 
+              data-caption='
+              <b>Цена: 62600 руб</b><br>
+              <b>Сроки: 4 дня</b><br>
+              <b>Заказ: </b> 20 Стрейч-Чехлов на коктельный столы, 6 Стрейч-Чехлов на фуршетные столы, 2 круглые Скатерти диаметром 326 сантиметров + 20 стрейч-чехлов на стулья<br>' alt="">
+              <img class="fotorama__img" src="img/7_cases/333.jpg" data-caption='
+              <b>Цена: 126000 руб</b><br>
+              <b>Сроки: 7 дней</b><br>
+              <b>Заказ: </b>30 комплектов текстиля для ресторана "Сквер" 
+              <br>Состав комплекта: Скатерть 150х120, 2 Наперона 90х90, 8 Салфеток <br>'
+               alt="">
+              <img class="fotorama__img" src="img/7_cases/444.jpg" data-caption='
+              <b>Цена: 61800 руб</b><br>
+              <b>Сроки: 2 недели</b><br>
+              <b>Заказ: </b>12 комплектов униформы для официантов в ресторан "Сквер"  <br>
+              1 Комплект это: Жилетка, рубашка, галстук или бабочка, брюки или юбка<br> 1 комплект это: Жилетка, рубашка, галстук или бабочка, брюки или юбка<br>' alt="">
+              <img class="fotorama__img" src="img/7_cases/500.jpg" data-caption='
+              <b>Цена: 102000 руб</b><br>
+              <b>Сроки: 10 дней</b><br>
+              <b>Заказ: </b>Полных коплект для Кейтеринговых компани: 15 Скатертей 300 сантиметров, 300 салфеток + 30 Фартуков для официантов с Фирменным логотипом' alt="">
+              <img class="fotorama__img" src="img/7_cases/555.jpg" data-caption='
               <b>Цена: 7600 руб</b><br>
               <b>Сроки: 2 дня</b><br>
-              	' alt="">
-              <img class="fotorama__img" src="img/7_cases/222.jpg" data-caption='15 стрейч чехлов на коктельные столы, 10 на фуршетные, 32 стрейч чехла на стулья <br>
-              <b>Цена: 10300 руб</b><br>
-              <b>Сроки: 2 дня</b><br>' alt="">
-              <img class="fotorama__img" src="img/7_cases/333.jpg" data-caption='30 комплектов текстиля для ресторана "Голубые огни". <br> 1 комплект это: скатерть, наперон, 8 салфеток, 4 чехла на стулья <br>
-              <b>Цена: 27400 руб</b><br>
-              <b>Сроки: 3 дня</b><br>' alt="">
-              <img class="fotorama__img" src="img/7_cases/444.jpg" data-caption='12 комплектов одежды для офицантов в тот же ресторан "Голубые огни". <br> 1 комплект это: Жилетка, рубашка, галстук или бабочка, брюки или юбка.<br>
-              <b>Цена: 17600 руб</b><br>
-              <b>Сроки: 2 дня</b><br>' alt="">
-              <img class="fotorama__img" src="img/7_cases/500.jpg" data-caption='полный комплект. Скатерти, одежда для офицантов, салфетки <br>
-              <b>Цена: 7600 руб</b><br>
-              <b>Сроки: 2 дня</b><br>' alt="">
-              <img class="fotorama__img" src="img/7_cases/555.jpg" data-caption='10 комплектов одежды для поворов в ресторан "Коричневый берег". <br>
-              <b>Цена: 7600 руб</b><br>
-              <b>Сроки: 2 дня</b><br>' alt="">
-              <img class="fotorama__img" src="img/7_cases/600.jpg" data-caption='30 стрейч чехлов на коктельные столы и 10 на прямоугольные <br>
-              <b>Цена: 7600 руб</b><br>
-              <b>Сроки: 2 дня</b><br>' alt="">
-              <img class="fotorama__img" src="img/7_cases/666.jpg" data-caption='12 комплектов одежды для офицантов в ресторан "Коричневый берег". <br> 1 комплект это: Жилетка, рубашка, галстук или бабочка, брюки или юбка.<br>
-              <b>Цена: 7600 руб</b><br>
-              <b>Сроки: 2 дня</b><br>' alt="">
-              <img class="fotorama__img" src="img/7_cases/777.jpg" data-caption='30 стрейч чехлов на коктельные столы и 10 на прямоугольные <br>
-              <b>Цена: 7600 руб</b><br>
-              <b>Сроки: 2 дня</b><br>' alt="">
+              <b>Заказ: </b>25 Комплектов для поваров ресторана Nagasaki <br>Коплект: Повызка на готову, Кимоно, фартук, брюки<br>' alt="">
+              <img class="fotorama__img" src="img/7_cases/600.jpg" data-caption='
+              <b>Цена: 308000 руб без логотипа / 76250 руб с логотипом</b><br>
+              <b>Сроки: 25 дней</b><br>
+              <b>Заказ: </b>Текстиль для ресторана гостиницы Marco Polo Presnya <br>
+              40 комплектов - скатерть 270 см + наперон 140 см - Ткань Италия под Заказ' alt="">
+              
+              <img class="fotorama__img" src="img/7_cases/666.jpg" data-caption='
+              <b>Цена: 750 руб - стандартный фартук, 550 руб - короткий фартук, 950 руб для бармена</b><br>
+              <b>Сроки: 8 часов</b><br>
+              <b>Заказ: </b> Изменение фасона фартука: официант мужчина и официант девушка, бармен' alt="">
+              <img class="fotorama__img" src="img/7_cases/777.jpg" data-caption='
+              <b>Цена: 108000 руб</b><br>
+              <b>Сроки: 2 дня</b><br>
+              <b>Заказ: </b>Фуршетный текстиль для Мастерской Вкуса. <br> 40 чехлов для Коктельных столов, 20 стрейч-чехлов для фуршетных прямоугольных столов, Шапки для коктельного стола 20 единиц' alt="">
+              
               <img class="fotorama__img" src="img/7_cases/800.jpg" data-caption='Наряды для хора 20 шт <br>
-              <b>Цена: 7600 руб</b><br>
-              <b>Сроки: 2 дня</b><br>' alt="">
+              <b>Цена: Сарафан 6500 руб / Косоворотка 4400 руб</b><br>
+              <b>Сроки: 3 недели</b><br>
+              <b>Заказ: </b>Стилизованная одежда для солистов хора. Сарафан и Косоворотка. Пошито 30 костюмов.' alt="">
               <img class="fotorama__img" src="img/7_cases/888.jpg" data-caption='Форма для горничных 400 шт <br>
-              <b>Цена: 7600 руб</b><br>
-              <b>Сроки: 2 дня</b><br>' alt="">
+              <b>Цена: 66500 руб</b><br>
+              <b>Сроки: 3 недели</b><br>
+              <b>Заказ: </b>Униформа для горничных в Moscow Country Club. Блузон + Брюки 35 комплектов' alt="">
             </div>
           </div>
           <!-- /.individually-slider -->
