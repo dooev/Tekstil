2223bc101b8d8ddeb3e4934648710bf49e73880e
Dima Dooev <dooevdima3@gmail.com>
2018-09-19 11:54:40 +0300

    Портфолио, переписал тексты

--- 6.0_individually.php
+++ 6.0_individually.php
@@ -8,10 +8,10 @@
         <div class="col-12 col-md-10 offset-md-1">
           <div class="individually-slider">
             <div class="fotorama" data-nav="thumbs" data-thumbwidth="115" data-thumbheight='78' data-transition="slide" data-loop="true" data-navwidth='100%' data-thumbmargin='7' data-arrows='always'>
+              <img class="fotorama__img" src="img/6.0_individually/4.jpg" alt="">
               <img class="fotorama__img" src="img/6.0_individually/1.jpg" alt="">
               <img class="fotorama__img" src="img/6.0_individually/2.jpg" alt="">
               <img class="fotorama__img" src="img/6.0_individually/3.jpg" alt="">
-              <img class="fotorama__img" src="img/6.0_individually/4.jpg" alt="">
               <img class="fotorama__img" src="img/6.0_individually/5.jpg" alt="">
               <img class="fotorama__img" src="img/6.0_individually/6.jpg" alt="">
               <!-- <img class="fotorama__img" src="img/6.0_individually/7.jpg" alt=""> -->
