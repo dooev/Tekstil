94132185a02f0cf868eed568e5531d605dfe993e
Dima Dooev <dooevdima3@gmail.com>
2018-09-19 09:57:55 +0300

    блок с тканями. каринки и тексты поправил

--- 5_material.php
+++ 5_material.php
@@ -7,9 +7,9 @@
       <div class="col-12 col-sm-6 col-md-4">
         <div class="material-block">
           <img class="material-block__img image" src="img/5_material/1-min.png" alt="Сатин">
-          <div class="material-block__title">Сатин
+          <div class="material-block__title">Испания
           </div>
-          <div class="material-block__subtitle">Основная ткань для пошива столового белья. Матовая, долговечная. Цена различная, в зависимости от поставщика.
+          <div class="material-block__subtitle">Особенностью считается практичный состав 50% Хлопок / 50% Полиэстер .
           </div>
           <!-- /.material-block__title -->
         </div>
@@ -19,9 +19,11 @@
       <div class="col-12 col-sm-6 col-md-4">
         <div class="material-block">
           <img class="material-block__img image" src="img/5_material/2-min.png" alt="Сатин">
-          <div class="material-block__title">Сатин
+          <div class="material-block__title">Италия
           </div>
-          <div class="material-block__subtitle">Основная ткань для пошива столового белья. Матовая, долговечная. Цена различная, в зависимости от поставщика.
+          <div class="material-block__subtitle">Особенностью считается ширина ткани 
+340 сантиметров
+Что обеспечивает возможность пошива Скатерти с диаметром 330 см, впол.
           </div>
           <!-- /.material-block__title -->
         </div>
@@ -31,9 +33,10 @@
       <div class="d-none d-md-block col-md-4">
         <div class="material-block">
           <img class="material-block__img image" src="img/5_material/3-min.png" alt="Сатин">
-          <div class="material-block__title">Сатин
+          <div class="material-block__title">Сирия
           </div>
-          <div class="material-block__subtitle">Основная ткань для пошива столового белья. Матовая, долговечная. Цена различная, в зависимости от поставщика.
+          <div class="material-block__subtitle">
+Сирийский сатен по праву считается классической тканью для банкетных юбок, полностью синтетический состав обеспечивает отличные эксплуатационные свойства
           </div>
           <!-- /.material-block__title -->
         </div>
@@ -46,9 +49,10 @@
       <div class="col-12 col-sm-6 col-md-4">
         <div class="material-block">
           <img class="material-block__img image" src="img/5_material/4-min.png" alt="Сатин">
-          <div class="material-block__title">Сатин
+          <div class="material-block__title">Турция
           </div>
-          <div class="material-block__subtitle">Основная ткань для пошива столового белья. Матовая, долговечная. Цена различная, в зависимости от поставщика.
+          <div class="material-block__subtitle">
+Практичный состав и разнообразие дизайнов делает ткани Турецких фабрик наиболее подходящими для Индивидуальных Дизайн-проектов
           </div>
           <!-- /.material-block__title -->
         </div>
@@ -58,9 +62,10 @@
       <div class="col-12 col-sm-6 col-md-4">
         <div class="material-block">
           <img class="material-block__img image" src="img/5_material/5-min.png" alt="Сатин">
-          <div class="material-block__title">Сатин
+          <div class="material-block__title">Россия
           </div>
-          <div class="material-block__subtitle">Основная ткань для пошива столового белья. Матовая, долговечная. Цена различная, в зависимости от поставщика.
+          <div class="material-block__subtitle">
+Натуральные Ткани: Хлопок, Лён. Всегда новые коллекции и дизайны
           </div>
           <!-- /.material-block__title -->
         </div>
@@ -70,9 +75,9 @@
       <div class="d-none d-md-block col-md-4">
         <div class="material-block">
           <img class="material-block__img image" src="img/5_material/6-min.png" alt="Сатин">
-          <div class="material-block__title">Сатин
+          <div class="material-block__title">Беларусь
           </div>
-          <div class="material-block__subtitle">Основная ткань для пошива столового белья. Матовая, долговечная. Цена различная, в зависимости от поставщика.
+          <div class="material-block__subtitle">Особенность считается Полиэфирная нить, которая обеспечивает постоянные Водоотталкивающие свойства.
           </div>
           <!-- /.material-block__title -->
         </div>
